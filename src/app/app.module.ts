9fb69b76f05bb0698560001afd877e377ee433ce
Tomas Kriska <kriskatomas360@gmail.com>
2021-03-06 17:30:35 +0100

    moved component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ExpencesComponent } from './pages/expences/expences.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent, ExpencesComponent, ExpensesComponent],
+  declarations: [AppComponent, LoginComponent, DashboardComponent, ExpensesComponent],
   imports: [BrowserModule, RouterModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
