5a871040dbc1e998953894ab3e4eb5a35a654875
Tomas Kriska <kriskatomas360@gmail.com>
2021-03-06 17:39:20 +0100

    separated mat module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ExpensesComponent } from './pages/expenses/expenses.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppMaterialModule } from './app-material.module';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, DashboardComponent, ExpensesComponent],
-  imports: [BrowserModule, RouterModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    ExpensesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AppMaterialModule,
+  ],
+  exports: [AppMaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
