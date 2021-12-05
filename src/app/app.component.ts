f854fafec882d48e85f462d70abe930eadaa8056
Tomas Kriska <tomas.kriska@nanoenergies.cz>
2021-12-05 01:58:44 +0100

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,46 +27,38 @@ export class AppComponent {
   from: string;
   to: string;
 
-  ngOnInit() {
-    this.oidcSecurityService.checkAuth().toPromise()
-      .then(({ isAuthenticated, userData, accessToken }) => {
-        if (isAuthenticated) {
-          this.isAuthenticated = isAuthenticated;
-        } else {
-          this.login();
+  async ngOnInit() {
+    let authResult = await this.oidcSecurityService.checkAuth().toPromise();
+    if (authResult.isAuthenticated) {
+      this.isAuthenticated = authResult.isAuthenticated;
+    } else {
+      this.login();
+    }
+
+    let account = await this.accountService.getAccount();
+    this.accountId = account.id;
+    let params = this.route.snapshot.queryParams
+    let nullOrEmpty = (x: string) => x == null || x == '';
+    this.budgetId = params['budgetId'];
+    this.from = params['from'];
+    this.to = params['to'];
+
+    if (nullOrEmpty(params['budgetId']) && nullOrEmpty(params['from'])) {
+      console.log('setting query parameters');
+      let navigation = await this.navigationService.getNavigation(this.accountId)
+
+      this.from = this.datePipe.transform(navigation.from, 'yyyy-MM-dd');
+      this.to = this.datePipe.transform(navigation.to, 'yyyy-MM-dd');
+
+      this.router.navigate([], {
+        relativeTo: this.route,
+        queryParams: {
+          budgetId: navigation.budgetId,
+          from: this.from,
+          to: this.to
         }
-
-        this.accountService.getAccount()
-          .then(a => {
-            this.accountId = a.id;
-            this.route.queryParams.subscribe(params => {
-
-              let nullOrEmpty = (x: string) => x == null || x == '';
-
-              this.budgetId = params['budgetId'];
-              this.from = params['from'];
-              this.to = params['to'];
-
-              if (nullOrEmpty(params['budgetId']) && nullOrEmpty(params['from'])) {
-                console.log('this needs setting');
-                this.navigationService.getNavigation(this.accountId)
-                  .then(result => {
-                    this.from = this.datePipe.transform(result.from, 'yyyy-MM-dd');
-                    this.to = this.datePipe.transform(result.to, 'yyyy-MM-dd');
-
-                    this.router.navigate([], {
-                      relativeTo: this.route,
-                      queryParams: {
-                        budgetId: result.budgetId,
-                        from: this.from,
-                        to: this.to
-                      }
-                    });
-                  });
-              }
-            })
-          })
       });
+    }
   }
 
   login() {
